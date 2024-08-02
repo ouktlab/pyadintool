3e971e910d58f39367caf0bdbaed665c81029c3f
rt <you@example.com>
2024-08-02 11:26:41 +0900

    add

--- setup_win.bat
+++ setup_win.bat
@@ -2,3 +2,4 @@ python -m pip install --upgrade pip
 pip install torch torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install numpy==1.26.4 pyyaml sounddevice huggingface_hub safetensors
 pip install PySide6 pyqtgraph
+pip install silero_vad
